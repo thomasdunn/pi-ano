e976e88d3cc66cb682655c6b6bced463c6622af7
Tom Dunn <tdunn@artofcontext.com>
2021-03-12 13:27:49 -0500

    Convert scale tonic and note duration to selects.  Styling for better control layout and more responsive sizing.  Queue notes into just three batches, first smaller to get music started faster.  Only three so we don't have many pauses during listening, just two.  Clear notes on stop.

--- src/index.js
+++ src/index.js
@@ -28,6 +28,8 @@ const noteDurations = [
     {name: 'dotted half (3/4)', duration: 3/4},
     {name: 'whole (1)', duration: 1}
 ];
+const c4Index = 39;
+const c7Index = 75;
 
 let currentNote = 0;
 let notes = [];
@@ -45,27 +47,21 @@ const piano = new Piano({
     }
 }).toDestination()
 
+let noteBatchTimeoutIds = [];
+
 const button = document.querySelector('button');
 const numEl = document.querySelector('#number');
 const scaleSelect = document.querySelector('#scale');
 const bpmRange = document.querySelector('#bpm');
 const bpmNumber = document.querySelector('#bpm-number');
-const tonicRange = document.querySelector('#tonic');
-const tonicLabel = document.querySelector('#tonic-label');
-const noteDurationRange = document.querySelector('#note-duration');
-const noteDurationLabel = document.querySelector('#note-duration-label');
+const tonicSelect = document.querySelector('#tonic');
+const noteDurationSelect = document.querySelector('#note-duration');
 
 bpmRange.addEventListener('input', () => {
     bpmNumber.value = bpmRange.value;
 });
 bpmNumber.addEventListener('input', () => {
     bpmRange.value = bpmNumber.value;
-});
-tonicRange.addEventListener('input', () => {
-    tonicLabel.innerText = allPianoNotes[tonicRange.value];
-});
-noteDurationRange.addEventListener('input', () => {
-    noteDurationLabel.innerText = noteDurations[noteDurationRange.value].name;
 });
 
 getScalesByLength().forEach(([length, scales]) => {
@@ -86,11 +82,32 @@ getScalesByLength().forEach(([length, scales]) => {
     scaleSelect.appendChild(scaleOptionGroup);
 });
 
+for (let i = 0; i <= c7Index; i++) {
+    const tonicOption = document.createElement('option');
+    tonicOption.value = i;
+    tonicOption.innerText = allPianoNotes[i];
+    if (i === c4Index) {
+        tonicOption.selected = true;
+    }
+    tonicSelect.appendChild(tonicOption);
+}
+
+for (let i = 0; i <= 5; i++) {
+    const noteDurationOption = document.createElement('option');
+    noteDurationOption.value = noteDurations[i].duration;
+    const text = noteDurations[i].name;
+    noteDurationOption.innerText = text;
+    if (text.startsWith('quarter')) {
+        noteDurationOption.selected = true;
+    }
+    noteDurationSelect.appendChild(noteDurationOption);
+}
+
 const build = () => {
     const scale = scaleSelect.value;
     const bpm = bpmRange.value;
-    const tonic = allPianoNotes[tonicRange.value];
-    const noteDuration = noteDurations[noteDurationRange.value].duration;
+    const tonic = allPianoNotes[tonicSelect.value];
+    const noteDuration = parseFloat(noteDurationSelect.value);
     const beatLengthSeconds = 60 / bpm;
     const digitDisplayDelayMs = 0.25 * beatLengthSeconds * 1000;
 
@@ -102,6 +119,8 @@ const build = () => {
     };
 
     const queueNotes = (newNotes) => {
+
+        console.log('queuing notes...');
 
         const noteOffNotes = newNotes.map(n => ({
             note: n.note,
@@ -128,18 +147,29 @@ const build = () => {
         notes.push(...newNotes);
     };
 
-    // to start queue up 1000 digits to get it started quickly
-    queueNotes(getNotes(options, 0, 1000));
+    // queue small batch to get music started faster...
+    // then two more batches for the remainder of available digits
 
-    // on some timeouts queue more batches of notes
-    for (let i = 1; i < 10; i++) {
+    const total = 10000;
+    const firstBatchSize = 1800;
+    const secondaryBatchesSize = (total - firstBatchSize) / 2;
+    const secondBatchTimeoutMs = 1000 * ((firstBatchSize * beatLengthSeconds) - 15);
+    const thirdBatchTimeoutMs = secondBatchTimeoutMs + (1000 * ((secondaryBatchesSize * beatLengthSeconds) - 15));
+
+    queueNotes(getNotes(options, 0, firstBatchSize)); // 1800 = 2 minutes at 900bpm
+
+    noteBatchTimeoutIds.push(
         setTimeout(
-            () => queueNotes(getNotes(options, i * 1000, (i + 1) * 1000)),
-            // 1000 notes at the max 1200 bpm takes 50 seconds
-            // so queue up another batch a while before that...
-            i * 40000
-        );
-    }
+            () => queueNotes(getNotes(options, firstBatchSize, firstBatchSize + secondaryBatchesSize)),
+            secondBatchTimeoutMs
+        )
+    );
+    noteBatchTimeoutIds.push(
+        setTimeout(
+            () => queueNotes(getNotes(options, firstBatchSize + secondaryBatchesSize, total)),
+            thirdBatchTimeoutMs
+        )
+    );
 
 };
 
@@ -168,6 +198,9 @@ function toggle(running) {
             tonePart.dispose();
         });
         toneParts = [];
+        notes = [];
+        noteBatchTimeoutIds.forEach(id => clearTimeout(id));
+        noteBatchTimeoutIds = [];
     }
     else {
         numEl.innerHTML = '';
